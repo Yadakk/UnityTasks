7590d399d97a9684463b919d99843d24d4604131
Yadakk <lolyer47@gmail.com>
2024-10-29 15:01:01 +0300

    refactor: Replaced invoke with coroutine in EnemySpawner

--- Assets/Task1_ObjectPool/Source/GameAssembly/EnemySpawner.cs
+++ Assets/Task1_ObjectPool/Source/GameAssembly/EnemySpawner.cs
@@ -1,3 +1,5 @@
+using Codice.Client.Common.GameUI;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,20 +16,50 @@ namespace ObjectPool
         [SerializeField]
         private int maxEnemies = 10;
 
+        private Mode mode;
+
+        private enum Mode
+        {
+            Spawning,
+            Despawning,
+        }
+
         void Start()
         {
-            InvokeRepeating(nameof(Spawn), 0f, 1f);
+            SpawnThenDespawnEnemies();
+        }
+
+        private void SpawnThenDespawnEnemies()
+        {
+            StartCoroutine(Routine());
+
+            IEnumerator Routine()
+            {
+                while (true)
+                {
+                    switch(mode)
+                    {
+                        case Mode.Spawning: Spawn(); break;
+                        case Mode.Despawning: Despawn(); break;
+                    }
+
+                    CheckModeChange();
+
+                    yield return new WaitForSeconds(1f);
+                }
+            }
+        }
+
+        private void CheckModeChange()
+        {
+            if (enemies.Count == 0)
+                mode = Mode.Spawning;
+            else if (enemies.Count == maxEnemies)
+                mode = Mode.Despawning;
         }
 
         private void Spawn()
         {
-            if (enemies.Count == maxEnemies)
-            {
-                CancelInvoke(nameof(Spawn));
-                InvokeRepeating(nameof(Despawn), 0f, 1f);
-                return;
-            }
-
             var enemy = prototype.Instantiate<Enemy>();
             enemies.Enqueue(enemy);
             enemy.Say("Hello");
@@ -35,13 +67,6 @@ namespace ObjectPool
 
         private void Despawn()
         {
-            if (enemies.Count == 0)
-            {
-                CancelInvoke(nameof(Despawn));
-                InvokeRepeating(nameof(Spawn), 0f, 1f);
-                return;
-            }
-
             var enemy = enemies.Dequeue();
             enemy.Say("Goodbye");
             enemy.ReturnToPool();
